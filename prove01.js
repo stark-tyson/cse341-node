5a3e70af6b566e35f8b6a04a8d65ceb820b9d726
Tyson Stark <starktj22@gmail.com>
2021-09-16 14:35:45 -0700

    assign 1 commit

--- prove01.js
+++ prove01.js
@@ -1,14 +1,7 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    // process.exit(); Hard exits the event loop
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>First server</title></head>');
-    res.write('<body><h1>Hello! Let us see how this Node.js stuff works!</h1></body>');
-    res.write('</html>');
-    res.end();
-});
+const routes = require('./prove01-routes');
+
+const server = http.createServer(routes);
 
 server.listen(3000);

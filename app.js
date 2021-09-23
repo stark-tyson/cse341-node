784ae72bffad08bd54d1cff729758deba7b442cc
Tyson Stark <starktj22@gmail.com>
2021-09-23 11:36:40 -0700

    prove02 commit

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
+const path = require('path');
 const express = require('express');
 const bParser = require('body-parser');
 const app = express();
+
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', 'notFound.html'));
 });
 
 

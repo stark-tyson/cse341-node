37ab5d677db0bb013801ec0718ca32104386b6da
Tyson Stark <starktj22@gmail.com>
2021-09-24 16:42:01 -0700

    commit template examples

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 const path = require('path');
 const express = require('express');
 const bParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 const app = express();
 
 
-const adminRoutes = require('./routes/admin');
+// app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
+// app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+//app.engine('pug', require('pug').__express)
+
+
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', 'notFound.html'));
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
 
--- routes/admin.js
+++ routes/admin.js
@@ -3,13 +3,17 @@ const express = require('express');
 const rootDir = require('../util/path');
 const router = express.Router();
 
+const products = [];
+
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product', formsCSS: true, productCSS: true, activeAddProduct: true });
 });
 
 router.post('/add-product', (req, res, next) => {
+    products.push({ title: req.body.title })
     console.log(req.body);
     res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;

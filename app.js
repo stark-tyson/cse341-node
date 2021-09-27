d88d22baf64370ee0a763f85024b7647489f0d8e
Tyson Stark <starktj22@gmail.com>
2021-09-27 15:24:06 -0700

    MVC commit

--- app.js
+++ app.js
@@ -5,26 +5,23 @@ const bParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000; // So we can run on heroku || (OR) localhost:3000
 
+const errorController = require('./controllers/error');
 //  app.engine('hbs', expressHbs({layoutsDir: 'practice/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
 //  app.set('view engine', 'hbs');
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 //app.engine('pug', require('pug').__express)
 
-
-
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'});
-});
+app.use(errorController.get404);
 
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));

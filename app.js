ce1d3e681c259142f3e1a9d2deba6d19d4298457
Tyson Stark <starktj22@gmail.com>
2021-09-27 13:58:34 -0700

    practice commit

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const path = require('path');
 const express = require('express');
 const bParser = require('body-parser');
-const expressHbs = require('express-handlebars');
+//const expressHbs = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT || 3000; // So we can run on heroku || (OR) localhost:3000
 
-//  app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
+//  app.engine('hbs', expressHbs({layoutsDir: 'practice/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
 //  app.set('view engine', 'hbs');
 app.set('view engine', 'ejs');
 app.set('views', 'views');

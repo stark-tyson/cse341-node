6d1b9b62a45f3a314f21e9a3c692445dece4610d
Tyson Stark <starktj22@gmail.com>
2021-09-30 17:30:09 -0700

    prove03 commit

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,18 @@
 const path = require('path');
 const express = require('express');
-const productsController = require('../controllers/products');
+const adminController = require('../controllers/admin');
 const router = express.Router();
 
-router.get('/add-product', productsController.getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
 
-router.post('/add-product', productsController.postAddProduct);
+router.get('/products', adminController.getProducts);
+
+router.post('/add-product', adminController.postAddProduct);
+
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
+router.post('/edit-product', adminController.postEditProduct);
+
+router.post('/delete-product', adminController.postDeleteProduct);
 
 module.exports = router;

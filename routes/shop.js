9be20777f7375d3d0a9afa88db37918632ef9d47
Tyson Stark <starktj22@gmail.com>
2021-10-05 12:46:47 -0700

    Commit beforeDB

--- routes/shop.js
+++ routes/shop.js
@@ -1,7 +1,8 @@
 const path = require('path');
-const shopController = require('../controllers/shop');
 
 const express = require('express');
+
+const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
@@ -17,9 +18,8 @@ router.post('/cart', shopController.postCart);
 
 router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
+router.post('/create-order', shopController.postOrder);
+
 router.get('/orders', shopController.getOrders);
 
-router.get('/checkout', shopController.getCheckout);
-
 module.exports = router;
-

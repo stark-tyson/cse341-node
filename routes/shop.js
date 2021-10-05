c26d87ad0c1f58a3b41da2b37a6577367a06a6af
Tyson Stark <starktj22@gmail.com>
2021-10-05 13:26:08 -0700

    commit reverse

--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,7 @@
 const path = require('path');
+const shopController = require('../controllers/shop');
 
 const express = require('express');
-
-const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
@@ -18,8 +17,9 @@ router.post('/cart', shopController.postCart);
 
 router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
-router.post('/create-order', shopController.postOrder);
-
 router.get('/orders', shopController.getOrders);
 
+router.get('/checkout', shopController.getCheckout);
+
 module.exports = router;
+

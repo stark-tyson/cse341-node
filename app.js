f7b9388403c6317e7771c7aad45b9efcd00fad2a
Tyson Stark <starktj22@gmail.com>
2021-10-14 15:53:46 -0700

    session&cookies commit

--- app.js
+++ app.js
@@ -1,37 +1,57 @@
 const path = require('path');
 const PORT = process.env.PORT || 3000; // So we can run on heroku || (OR) localhost:3000
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://Tyson:PlyPwIqfRkRHSZeJ@cluster0.2xac1.mongodb.net/shop?retryWrites=true&w=majority";
 
 const express = require('express');
-const bParser = require('body-parser');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 const cors = require('cors');
 const errorController = require('./controllers/error');
-
+const User = require('./models/user');
 
 const app = express();
+const store = new MongoDBStore({
+    uri: MONGODB_URL,
+    collection: 'sessions'
+});
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const User = require('./models/user');
-
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
-app.use(bParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    session({
+        secret: 'my secret',
+        resave: false,
+        saveUninitialized: false,
+        store: store
+    })
+);
 
 app.use((req, res, next) => {
-    User.findById('6160ba831876f2d08fe37b60')
+    if (!req.session.user) {
+       return next();
+    }
+    User.findById(req.session.user._id)
         .then(user => {
             req.user = user;
             next();
         })
         .catch(err => console.log(err));
-    //next();
 });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
+
 app.use(errorController.get404);
 
 const corsOptions = {
@@ -40,7 +60,6 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://Tyson:PlyPwIqfRkRHSZeJ@cluster0.2xac1.mongodb.net/shop?retryWrites=true&w=majority";
 
 
 mongoose
---
+++ controllers/auth.js
@@ -0,0 +1,30 @@
+const User = require('../models/user');
+
+exports.getLogin = (req, res, next) => {
+    console.log(req.session.isLoggedIn);
+    res.render('auth/login', {
+        path: '/login',
+        pageTitle: 'Login',
+        isAuthenticated: false
+    });
+};
+
+exports.postLogin = (req, res, next) => {
+    User.findById('6160ba831876f2d08fe37b60')
+        .then(user => {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            req.session.save((err) => {
+                console.log(err);
+                res.redirect('/');
+            });
+        })
+        .catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy(err => {
+        console.log(err);
+        res.redirect('/');
+    });
+};
